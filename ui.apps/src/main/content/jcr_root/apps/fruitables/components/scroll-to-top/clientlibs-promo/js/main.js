19fc4d51ce98f63499f9f517418ccba121b0119c
AdityaYadav7744 <ayadav447799@gmail.com>
2026-04-01 17:27:08 +0530

    Updated the changes

--- ui.apps/src/main/content/jcr_root/apps/fruitables/components/scroll-to-top/clientlibs-promo/js/main.js
+++ ui.apps/src/main/content/jcr_root/apps/fruitables/components/scroll-to-top/clientlibs-promo/js/main.js
@@ -3,7 +3,6 @@ document.addEventListener("DOMContentLoaded", function () {
     const btn = document.getElementById("scrollTopBtn");
     if (!btn) return;
 
-    // Show button after scroll
     window.addEventListener("scroll", function () {
         if (window.scrollY > 200) {
             btn.classList.add("show");
@@ -15,15 +14,12 @@ document.addEventListener("DOMContentLoaded", function () {
     
     btn.addEventListener("click", function () {
 ``
-        // Add click effect
         btn.classList.add("clicked");
 
-        // Remove effect after short time
         setTimeout(function () {
             btn.classList.remove("clicked");
         }, 200);
 
-        // Scroll after delay
         setTimeout(function () {
             window.scrollTo({
                 top: 0,

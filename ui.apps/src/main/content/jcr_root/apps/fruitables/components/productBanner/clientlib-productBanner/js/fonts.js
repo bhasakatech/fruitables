7a552a425687d9fc7fbda4a38dd4d45ab729b2b9
iamtsreeharsha <sreeharsha2210@gmail.com>
2026-04-03 15:35:48 +0530

    made changes as requested
    added richtext and replaced import links in css to separate files

---
+++ ui.apps/src/main/content/jcr_root/apps/fruitables/components/productBanner/clientlib-productBanner/js/fonts.js
@@ -0,0 +1,10 @@
+var link1 = document.createElement("link");
+link1.href = "https://fonts.googleapis.com/css2?family=Raleway:wght@400;600;700&display=swap";
+link1.rel = "stylesheet";
+
+var link2 = document.createElement("link");
+link2.href = "https://fonts.googleapis.com/css2?family=Open+Sans:wght@300;400;500;600;700&display=swap";
+link2.rel = "stylesheet";
+
+document.head.appendChild(link1);
+document.head.appendChild(link2);

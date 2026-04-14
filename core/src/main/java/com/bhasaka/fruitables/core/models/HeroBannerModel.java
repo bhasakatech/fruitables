82f2a1cd353e854047fc28d3630ec55ece138749
iamtsreeharsha <sreeharsha2210@gmail.com>
2026-04-14 15:44:07 +0530

    minor fix in hero banner model

--- core/src/main/java/com/bhasaka/fruitables/core/models/HeroBannerModel.java
+++ core/src/main/java/com/bhasaka/fruitables/core/models/HeroBannerModel.java
@@ -1,12 +1,11 @@
 package com.bhasaka.fruitables.core.models;
 
-import java.util.Collections;
-import java.util.List;
-import org.apache.sling.api.resource.Resource;
-import org.apache.sling.models.annotations.DefaultInjectionStrategy;
-import org.apache.sling.models.annotations.Model;
+import org.apache.sling.models.annotations.*;
 import org.apache.sling.models.annotations.injectorspecific.ChildResource;
 import org.apache.sling.models.annotations.injectorspecific.ValueMapValue;
+import org.apache.sling.api.resource.Resource;
+import java.util.Collections;
+import java.util.List;
 
 @Model(
         adaptables = Resource.class,
